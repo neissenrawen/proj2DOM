0252e5e4948bc87a9194e8b447e71703c5eb928d
neissenrawen <neissenrawen35@gmail.com>
2025-07-09 08:56:47 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var colorBox = document.getElementById("color-box");
+  var changeColorBtn = document.getElementById("change-color-btn");
+
+  function getRandomColor() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  changeColorBtn.addEventListener("click", function () {
+    var newColor = getRandomColor();
+    console.log("Nouvelle couleur générée : " + newColor);
+    colorBox.style.backgroundColor = newColor;
+  });
+});
